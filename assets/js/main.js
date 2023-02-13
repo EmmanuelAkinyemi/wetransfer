271fcc39af681db5d70ad83818234d6d50e4b0f2
EmmanuelAkinyemi <93402402+EmmanuelAkinyemi@users.noreply.github.com>
2023-02-13 04:06:39 -0800

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,30 +8,50 @@ const myModal = new bootstrap.Modal("#pop-up"),
 downloadBtn.addEventListener("click", ()=>{
     downloadBox.classList.add("d-none");
     loadingBox.classList.remove("d-none");
+    load();
     console.log("the download btn was clicked")
 })
 
 
+function load(){
+    let progressStartValue = 0,
+      progressEndValue = 47,
+      speed = 200;
 
-let progressStartValue = 0,
-    progressEndValue = 47,
-    speed = 200;
+  let progress = setInterval(() => {
+    progressStartValue++;
 
-let progress = setInterval(() => {
-  progressStartValue++;
+    progressValue.textContent = `${progressStartValue}%`;
+    circularProgress.style.background = `conic-gradient(#5268FF ${
+      progressStartValue * 3.6
+    }deg, #ededed 0deg)`;
 
-  progressValue.textContent = `${progressStartValue}%`;
-  circularProgress.style.background = `conic-gradient(#5268FF ${
-    progressStartValue * 3.6
-  }deg, #ededed 0deg)`;
-
-  if (progressStartValue == progressEndValue) {
-    clearInterval(progress);
-    myModal.show()
-  }
-}, speed);
+    if (progressStartValue == progressEndValue) {
+      clearInterval(progress);
+      myModal.show()
+    }
+  }, speed);
+}
 
 
+// var i = 0;
+// function move() {
+//   if (i == 0) {
+//     i = 1;
+//     var elem = document.getElementById("myBar");
+//     var width = 1;
+//     var id = setInterval(frame, 10);
+//     function frame() {
+//       if (width >= 100) {
+//         clearInterval(id);
+//         i = 0;
+//       } else {
+//         width++;
+//         elem.style.width = width + "%";
+//       }
+//     }
+//   }
+// }
 //the pop-up modal
 // if (progressStartValue == progressEndValue) {
 //   window.addEventListener('DOMContentLoaded', ()=>{
